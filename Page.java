8957eca8b3c7e5020e4ca2ba2b8059327f5b8172
Amanda Wang <awviolin13@gmail.com>
2017-06-03 13:17:17 -0700

    added Page superclass

---
+++ Page.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.time.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Page{ //superclass for all the pages
+	public JButton backButton;
+	public JPanel panel;
+	public JLabel header;
+
+	
+	public Page(){
+		panel = new JPanel();	
+		backButton = new JButton("Back");
+		backButton.setActionCommand("back");
+		backButton.addActionListener(new ButtonClickListener());
+
+	}
+
+	public void display(){
+
+	}
+
+	public void remove(){ //called when back button is pressed
+
+	}
+
+}
