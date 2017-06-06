9f77bf1b59d10252b967575c15473e7e7103e94a
Apoorva Prakash <ap.prakash221@gmail.com>
2017-06-06 08:52:12 -0700

    asdflkasdflkj kill me

--- Page.java
+++ Page.java
@@ -7,28 +7,23 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.ArrayList;
-import java.io.File;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-
 
 public abstract class Page{ //superclass for all the pages
-	public JButton backButton;
+    public JButton backButton;
     public JButton playButton;
     public JFrame infoFrame;
-	public JPanel panel;
+    public JPanel panel;
     public JPanel infoPanel;
-	public JLabel header;
+    public JLabel header;
     public JLabel win;
-	public Timer timer;
+    public Timer timer;
     public Timer qtimer;
     public Timer atimer;
-	public JLabel timeDisplay;
+    public JLabel timeDisplay;
     public JLabel leaderInfo;
     public GridBagConstraints c;
     public ArrayList<TriviaQuestion> bank;
-    public int displayed = -1;
+    public int displayed = 0;
     public ArrayList<Boolean> answers;
     public ArrayList<LeaderAction> actions;
     public int qCounter = 0;
@@ -43,31 +38,29 @@ public abstract class Page{ //superclass for all the pages
     public int[] xPointsTwo = {10, 80, 90, 5};
     public int[] yPointsTwo = {110, 120, 140, 130};
 
-	
-	public Page(){
+    
+    public Page(){
         // information page for each leader
         infoFrame = new JFrame();
-		infoFrame.setSize(1000, 750);
+        infoFrame.setSize(1000, 750);
         infoPanel = new JPanel(new FlowLayout());
         leaderInfo = new JLabel();
         playButton = new JButton("Play!");
-		playButton.setActionCommand("play");
-		playButton.addActionListener(new ButtonClickListener());	
+        playButton.setActionCommand("play");
+        playButton.addActionListener(new ButtonClickListener());    
         infoPanel.add(leaderInfo);
         infoPanel.add(playButton);
         infoFrame.add(infoPanel);
         
         // game page
         waterLevel = 500;
-        // waterInterval = 25;
-		panel = new JPanel(new FlowLayout()){
+        waterInterval = 20;
+        panel = new JPanel(new FlowLayout()){
             @Override
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
                 Graphics2D g2 = (Graphics2D)g;
                 g2.setColor(new Color(25, 150, 200));
-                g2.fillRect(0, waterLevel, 1000, 750 - waterLevel);
-
                 g2.fillRect(0, waterLevel, 1000, 750-waterLevel);
                 g2.setColor(new Color(250, 215, 66));
                 g2.fillOval(10, 10, 70, 70);
@@ -79,9 +72,9 @@ public abstract class Page{ //superclass for all the pages
         };
         
         // back button to get to home page
-		backButton = new JButton("Back");
-		backButton.setActionCommand("back");
-		backButton.setPreferredSize(new Dimension(75, 30));
+        backButton = new JButton("Back");
+        backButton.setActionCommand("back");
+        backButton.setPreferredSize(new Dimension(75, 30));
         
         // co2 button
         co2 = new JButton("CO2");
@@ -94,7 +87,7 @@ public abstract class Page{ //superclass for all the pages
         ch4.addActionListener(new ButtonClickListener());
         
         // stopwatch with current "date"
-		timeDisplay = new JLabel();
+        timeDisplay = new JLabel();
         String dt = new SimpleDateFormat("EEE, d MMM yyyy").format(new Date()); // starts date as today
         SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy");
         Calendar cal = Calendar.getInstance();
@@ -103,7 +96,7 @@ public abstract class Page{ //superclass for all the pages
         timeDisplay.setText(sdf.format(cal.getTime()));
         
         // displays the "date", currently 10 days in 1 second
-       	timer = new Timer(100, new ActionListener() {
+        timer = new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 cal.add(Calendar.DATE, 1);
@@ -112,15 +105,6 @@ public abstract class Page{ //superclass for all the pages
             }
         });
         
-        // displays the trivia questions every 60 seconds
-        qtimer = new Timer(5000, new ActionListener() {
-            int i = 0;
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                TriviaQuestion test = bank.get(i);
-                test.displayQuestion();
-                displayed = i;
-                i++;
         // displays actions at fixed intervals
         atimer = new Timer(25000, new ActionListener() {
             int i = 0;
@@ -159,11 +143,11 @@ public abstract class Page{ //superclass for all the pages
         
         actions = new ArrayList<LeaderAction>();
         
-		panel.add(backButton);
+        panel.add(backButton);
         panel.add(co2);
         panel.add(ch4);
-		panel.add(timeDisplay);
-	}
+        panel.add(timeDisplay);
+    }
 
     public void addImage(String filename) {
         ImageIcon icon = new ImageIcon(filename);
@@ -173,25 +157,43 @@ public abstract class Page{ //superclass for all the pages
         label.setHorizontalAlignment(JLabel.RIGHT);
         label.setVerticalAlignment(JLabel.TOP);
         panel.add(label);
-	}
+    }
 
     public void closeTrivia(){
-        if (displayed == -1){
-            return;
-        }
-        for (int i = 0; i <= displayed; i++){
+        for (int i = 0; i < displayed; i++){
             bank.get(i).close();
         }
+    }
+
+    public void updateCounter(){
+        int counter = 0;
+        for (int i = 0; i < answers.size(); i++){
+            if (answers.get(i)){
+                counter++;
+            }
+        }
+        qCounter = counter;
+        System.out.println(qCounter);
+    }
+
+    public void resetCounter(){
+        qCounter = 0;
+        answers.clear();
+        displayed = 0;
     }
     
     public abstract void info();
     
+    public void closeInfo(){
+        infoFrame.dispatchEvent(new WindowEvent(infoFrame, WindowEvent.WINDOW_CLOSING));
+    }
+
     public abstract void win();
     
-	public void setHeader(String s){
-		header = new JLabel("Level " + s, JLabel.CENTER);
-		panel.add(header);
-	}
+    public void setHeader(String s){
+        header = new JLabel("Level " + s, JLabel.CENTER);
+        panel.add(header);
+    }
 
     public void waterLevelRising(){
         waterLevel -= waterInterval;
@@ -223,39 +225,35 @@ public abstract class Page{ //superclass for all the pages
 
 
 
-	public void resetWater(){
+    public void resetWater(){
         waterLevel = 500;
     }
     
-	public class ButtonClickListener implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-        	String command = e.getActionCommand();
+    public class ButtonClickListener implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            String command = e.getActionCommand();
             if (command.equals("co2")){
                 waterLevelRising();
-                // String soundName = "Honk.wav";    
-                // try {
-                //     Clip clip = AudioSystem.getClip();
-                //     AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(soundName).getAbsoluteFile());
-                //     clip.open(inputStream);
-                //     clip.start(); 
-                //     } catch (Exception a) {
-                //       System.err.println(a.getMessage());
-                //     }
-                //     panel.repaint();
-            } else if (command.equals("play")){
-            	infoFrame.dispatchEvent(new WindowEvent(infoFrame, WindowEvent.WINDOW_CLOSING));
+            }    
+            else if (command.equals("play")){
+                infoFrame.dispatchEvent(new WindowEvent(infoFrame, WindowEvent.WINDOW_CLOSING));
                 timer.start();
                 qtimer.start();
-            } else if (command.equals("ch4")){
-                waterLevel -= (3*waterInterval);
                 atimer.start();
             } 
             else if (command.equals("ch4")){
-                waterLevel -= 75;
+                waterLevel -= (3*waterInterval);
                 ch4.setEnabled(false);
                 panel.repaint();
             } 
-		}
-	}	
-
+            else if (command.equals("answered")){
+                for (int i = 0; i <= answers.size(); i++){
+                    if (bank.get(i).ansCorrectly){
+                        answers.set(i, Boolean.TRUE);
+                    }
+                }
+                updateCounter();
+            }
+        }
+    }   
 }
